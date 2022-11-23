c417c618052fc78da01a6c0f9cbee8dd7b5bab71
leonardv <valentin.leonard@outlook.fr>
2022-11-23 09:16:30 +0100

    Partie 1: Travail sur la BD: 1.3a : Créer les tables manquantes et modifier celles ci-dessous 1.3a : Détruire les tables manquantes et modifier celles ci-dessous

--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -1,5 +1,5 @@
--- TODO 1.3a : Créer les tables manquantes et modifier celles ci-dessous
-CREATE TABLE LesSportifsEQ
+-- DONETODO 1.3a : Créer les tables manquantes et modifier celles ci-dessous
+CREATE TABLE LesSportifs
 (
   numSp NUMBER(4),
   nomSp VARCHAR2(20),
@@ -7,10 +7,30 @@ CREATE TABLE LesSportifsEQ
   pays VARCHAR2(20),
   categorieSp VARCHAR2(10),
   dateNaisSp DATE,
-  numEq NUMBER(4),
-  CONSTRAINT SP_CK1 CHECK(numSp > 0),
-  CONSTRAINT SP_CK2 CHECK(categorieSp IN ('feminin','masculin')),
-  CONSTRAINT SP_CK3 CHECK(numEq > 0)
+  CONSTRAINT SP_PK PRIMARY KEY (numSp),
+  CONSTRAINT SP_CK1 CHECK(numSp > 999),
+  CONSTRAINT SP_CK2 CHECK(numSp < 1500),
+  CONSTRAINT SP_CK3 CHECK(categorieSp IN ('feminin','masculin'))
+);
+
+CREATE TABLE LesEquipes
+(
+  numSp NUMBER(4),
+  numEq NUMBER(3),
+  CONSTRAINT EQ_PK PRIMARY KEY (numSp, numEq),
+  CONSTRAINT EQ_CK1 CHECK(numEq > 0),
+  CONSTRAINT EQ_CK2 CHECK(numEq < 101),
+  FOREIGN KEY (numSp) REFERENCES LesSportifs(numSp)
+);
+
+CREATE TABLE LesParticipants
+(
+  numIns NUMBER(4),
+  numEp NUMBER(4),
+  CONSTRAINT PA_PK PRIMARY KEY (numIns, numEp),
+  FOREIGN KEY (numIns) REFERENCES LesSportifs(numSp),
+  FOREIGN KEY (numIns) REFERENCES LesEquipes(numEq),
+  FOREIGN KEY (numEp) REFERENCES LesEpreuves(numEp),
 );
 
 CREATE TABLE LesEpreuves
@@ -22,11 +42,17 @@ CREATE TABLE LesEpreuves
   categorieEp VARCHAR2(10),
   nbSportifsEp NUMBER(2),
   dateEp DATE,
+  res_or NUMBER(4),
+  res_argent NUMBER(4),
+  res_bronze NUMBER(4),
   CONSTRAINT EP_PK PRIMARY KEY (numEp),
   CONSTRAINT EP_CK1 CHECK (formeEp IN ('individuelle','par equipe','par couple')),
   CONSTRAINT EP_CK2 CHECK (categorieEp IN ('feminin','masculin','mixte')),
   CONSTRAINT EP_CK3 CHECK (numEp > 0),
-  CONSTRAINT EP_CK4 CHECK (nbSportifsEp > 0)
+  CONSTRAINT EP_CK4 CHECK (nbSportifsEp > 0),
+  FOREIGN KEY (res_or) REFERENCES LesParticipants(numIns),
+  FOREIGN KEY (res_argent) REFERENCES LesParticipants(numIns),
+  FOREIGN KEY (res_bronze) REFERENCES LesParticipants(numIns)
 );
 
 -- DONETODO 1.4a : ajouter la définition de la vue LesAgesSportifs
@@ -34,12 +60,12 @@ CREATE TABLE LesEpreuves
 CREATE VIEW IF NOT EXISTS LesAgesSportifs(numSp, age)
 AS
    SELECT numSp, strftime('%Y', 'now') - strftime('%Y', dateNaisSp)
-   FROM LesSportifsEQ
+   FROM LesSportifs
    WHERE strftime('%m', 'now') > strftime('%m', dateNaisSp) OR
          strftime('%m', 'now') = strftime('%m', dateNaisSp) AND strftime('%d', 'now') = strftime('%d', dateNaisSp)
    UNION
    SELECT numSp, strftime('%Y', 'now') - strftime('%Y', dateNaisSp) - 1
-   FROM LesSportifsEQ
+   FROM LesSportifs
    WHERE NOT(strftime('%m', 'now') > strftime('%m', dateNaisSp) OR
          strftime('%m', 'now') = strftime('%m', dateNaisSp) AND strftime('%d', 'now') = strftime('%d', dateNaisSp));
 
@@ -49,10 +75,11 @@ CREATE VIEW IF NOT EXISTS LesNbsEquipiers(numSp, nbEquipiers, numEq)
 AS
     WITH nbPerEq as(
         SELECT COUNT(numEq)-1 nbEquipiers, numEq
-        FROM LesSportifsEQ
+        FROM LesEquipes
         GROUP BY numEq
     )
     SELECT numSp, nbEquipiers, numEq
-    FROM LesSportifsEQ JOIN nbPerEq USING(numEq);
+    FROM LesSportifs JOIN LesEquipes USING(numSp)
+                     JOIN nbPerEq USING(numEq);
 
 -- TODO 3.3 : ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracle)
--- data/v1_deleteDB.sql
+++ data/v1_deleteDB.sql
@@ -1,6 +1,8 @@
--- TODO 1.3a : Détruire les tables manquantes et modifier celles ci-dessous
+-- DONETODO 1.3a : Détruire les tables manquantes et modifier celles ci-dessous
 DROP TABLE IF EXISTS LesEpreuves;
-DROP TABLE IF EXISTS LesSportifsEQ;
+DROP TABLE IF EXISTS LesSportifs;
+DROP TABLE IF EXISTS LesEquipes;
+DROP TABLE IF EXISTS LesParticipants;
 
 DROP VIEW IF EXISTS LesAgesSportifs;
 DROP VIEW IF EXISTS LesNbsEquipiers;
